35b0368bc83d4e16486758c6877ae7d29033ce9d
Alena <alena.nik.digital@gmail.com>
2020-09-20 19:47:11 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Composition />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Composition from './Composition.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Composition
   }
 }
 </script>
---
+++ src/Composition.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <p> Count: {{ count }} </p>
+        <p> A. {{ numbers.a }} </p>
+        <p> B: {{ numbers.b }} </p>
+        <p> Total : {{ total }} </p>
+      <button @click="increase('a')">Increase a</button>
+      <button @click="increase('b')">Increase b</button>
+      <button @click="increment"> + </button>
+      <button @click="decrement"> - </button>
+    </div>
+</template>
+
+<script>
+import { ref, reactive, computed } from 'vue';
+
+export default {
+  setup() {
+    // for a single value / number
+    const count = ref(0)
+    // for complex objects / values
+    const numbers = reactive({
+      a: 0,
+      b: 0
+    })
+
+    const increase = (num) => {
+        numbers[num] += 1
+    }
+
+    const increment = () => {
+        count.value += 1
+    }
+    const decrement = () => {
+        count.value -= 1
+    }
+
+    const total = computed(() => {
+       return count.value + numbers.a + numbers.b
+    })
+
+    return {
+        count,
+        increment,
+        decrement,
+        increase,
+        numbers,
+        total
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
